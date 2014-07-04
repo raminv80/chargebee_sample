7facfd4f938656043fbc8ee81e5b3cbb013bed1a
Ramin Vakilian <ramin@motionworks.com.my>
2014-07-04 15:05:55 +0800

    fix

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,7 @@ class Subscription < ActiveRecord::Base
 
   def self.find_or_create_subscription(email, subscription_id, customer_id)
   	user = User.where(email: email).first
-	subscriber = self.where({user.id, subscription_id: subscription_id}).first
+	subscriber = self.where({user_id: user.id, subscription_id: subscription_id}).first
 	return nil unless subscriber.nil?
 	subscriber = self.new(user_id: user.id, subscription_id: subscription_id, customer_id: customer_id)
 	subscriber.save!
