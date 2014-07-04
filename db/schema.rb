412cc51c6a3677ecbe895c4cef797ecfb5a6ed08
Ramin Vakilian <ramin@motionworks.com.my>
2014-07-04 13:55:59 +0800

    add time stamps to subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703025513) do
+ActiveRecord::Schema.define(version: 20140703025514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,11 @@ ActiveRecord::Schema.define(version: 20140703025513) do
   end
 
   create_table "subscriptions", force: true do |t|
-    t.integer "user_id"
-    t.string  "customer_id"
-    t.string  "subscription_id"
+    t.integer  "user_id"
+    t.string   "customer_id"
+    t.string   "subscription_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
