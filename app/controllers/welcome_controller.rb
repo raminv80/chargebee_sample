a67080760e8757dfc4265ffaddb8b4f8768f866f
Ramin Vakilian <ramin@motionworks.com.my>
2014-07-04 17:10:11 +0800

    fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -10,7 +10,20 @@ class WelcomeController < ApplicationController
     # For demo purpose plan with id 'basic' is hard coded here.
     if(!session[:user_id].nil?)
       user = User.find(session[:user_id])
-      @subscriptions = Subscription.where(user_id: user.id)
+      
+      subscriptions = Subscription.where(user_id: user.id)
+
+      @subscriptions = subscriptions.map do |subscription|
+        result = ChargeBee::Subscription.retrieve(subscription.subscription_id)
+        if(result.class==ChargeBee::Result)
+        {
+          customer: result.customer,
+          subscription: result.subscription,
+          card: result.card
+        } 
+        end
+      end
+
       if(@subscriptions.empty?)
         responseResult = ChargeBee::HostedPage.checkout_new({
           :subscription => {:plan_id=>"basic" }, 
