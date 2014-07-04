c28b3e82ca2aa884521dde174862de1930beaf0b
Ramin Vakilian <ramin@motionworks.com.my>
2014-07-04 16:54:20 +0800

    fix

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -15,10 +15,10 @@ class SubscriptionController < ApplicationController
         redirect_to "/400"
         return
       end
-      @customer = result.customer
-      @subscription = result.subscription
-      @card = result.card
-      handle_subscription(result)
+      @customer = result.hosted_page.content.customer
+      @subscription = result.hosted_page.content.subscription
+      @card = result.hosted_page.content.card
+      handle_subscription(@customer, @subscription)
     else
       #request comes from hosted checkout page
       result = ChargeBee::Subscription.retrieve(params[:sub_id])
@@ -26,7 +26,7 @@ class SubscriptionController < ApplicationController
         @customer = result.customer
         @subscription = result.subscription
         @card = result.card
-        handle_subscription(result)
+        handle_subscription(@customer, @subscription)
       else
        redirect_to "/400"
       end
@@ -41,10 +41,10 @@ class SubscriptionController < ApplicationController
   		:api_key => Settings[:mindvalley][:chargebee][:api_key])
   end
 
-  def handle_subscription(result)
-    subscription_id = result.subscription.id
-    customer_id = result.customer.id
-    email = result.customer.email
+  def handle_subscription(customer, subscription)
+    subscription_id = subscription.id
+    customer_id = customer.id
+    email = customer.email
     Subscription.find_or_create_subscription(email, subscription_id, customer_id)
   end
 end
